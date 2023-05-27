a9b619a71b4de916693f2d9fd46f604de9e92195
kesyasyawalani <100104547+kesyasyawalani@users.noreply.github.com>
2023-05-28 00:50:08 +0700

    Create doneController.php

---
+++ doneController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class doneController extends Controller
+{
+    public function openWorkspace(){
+    // mengambil data pegawai berdasarkan id yang dipilih
+    $task = DB::table('Task')->get();
+    $workspace = DB::table('Workspace')->get();
+
+    //'' utk diakses di view
+    return view('workspace',['task' => $task , 'workspace' => $workspace]);
+    }
+
+    /*public function doneTask(){
+        //read and insert checkbox in database
+        if(isset($_POST['submit'])){
+            //check checkbox slected or not
+            if(!empty($_POST['tasks'])){
+                $tasks = implode(",",$_POST['tasks']);
+
+                //insert and update record
+                $checkEntries = mysqli_query($con,"SELECT * FROM Task");
+              if(mysqli_num_rows($checkEntries) == 0){
+                   mysqli_query($con,"INSERT INTO finishedTask() VALUES('".$tasks."')");
+              }else{
+                   mysqli_query($con,"UPDATE Task SET language='".$tasks."' ");
+              }
+            }
+        }
+
+        return redirect('/workspace');
+    }*/
+
+}
