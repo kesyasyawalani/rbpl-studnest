dcc6f3b4990c5c42c73eee92ca6d30630c5f34c1
kesyasyawalani <100104547+kesyasyawalani@users.noreply.github.com>
2023-05-31 13:40:41 +0700

    Create web.php

---
+++ web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\doneController;
+use App\Http\Controllers\CreateWorkspaceController;
+use App\Http\Controllers\AddMytaskController;
+
+
+Route::get('/', function () {
+    return view('templateworkspace');
+});
+
+Route::controller(doneController::class)->group(function () {
+    Route::get('/workspace', 'openWorkspace');
+});
+
+Route::controller(CreateWorkspaceController::class)->group(function () {
+    Route::get('workspace/form', 'showFormWorkspace');
+    Route::post('workspace/addDataFormWorkspace','addDataFormWorkspace');
+});
+
+Route::controller(AddMytaskController::class)->group(function () {
+    Route::get('task/form', 'openAddMyTask');
+    Route::post('workspace/updateMyTask','updateMyTask');
+});
