d90aa048d37fc775891c61618fe7f99aa8fac2c0
kesyasyawalani <100104547+kesyasyawalani@users.noreply.github.com>
2023-06-08 10:55:44 +0700

    Update templateworkspace.blade.php

--- templateworkspace.blade.php
+++ templateworkspace.blade.php
@@ -1,33 +1,53 @@
-html, body{
-    background: linear-gradient(to top right, #A4C5FD4D, #8FC6FD4D,#E0C4FD4D);
-    font-family: 'Poppins', sans-serif;
-    height: 100vh;
-    margin: 0;
-}
+<!DOCTYPE html>
+<html>
 
-.row{
-    margin: 0;
-    height: 90vh;
-}
+<head>
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 
-.navbar-brand{
-    font-size: 100%;
-    color: black;
-}
-.nav-link{
-    color: #858585;
-}
-.choose{
-    background-color: white;
-    color: #5CA9FF;
-}
-.content{
-    background-color: rgba(255, 255, 255, 0.7);
-    min-height: max-content;
-    min-height: 100%;
-    border-radius: 30px 0px 0px 0px;
+    <!-- jQuery library -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
 
-    /*display: flex;
-    justify-content: center;
-    align-items: center;*/
-}
+    <!-- Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://use.fontawesome.com/releases/v6.4.0/css/all.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/master.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-sm">
+            <div class="container-fluid">
+                <a class="navbar-brand px-1" href="#">
+                    <img src="{{ asset('images/logo_studnest.png') }}" alt="StudNest" width="20%" height="20%"
+                        class="d-inline-block align-text-top">
+                    <b>StudNest</b>
+                </a>
+                <div class="navbar-nav ms-auto pr-5">
+                    <a>
+                        <i class="fas fa-user mr-3"></i>
+                        Nama
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <div class="row mt-2">
+        <div class="sidebar col-md-2">
+            @yield('sidebar')
+        </div>
+        <div class="content col-md-10 row-md-10">
+            @yield('isi')
+        </div>
+    </div>
+</body>
+
+</html>
